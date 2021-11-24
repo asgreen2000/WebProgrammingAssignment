47e05598aee0e5e2c0b180465bcf7d2db4519904
thanhluan <nguyenthanhluan596@gmail.com>
2021-11-24 12:12:53 +0700

    create UI

--- app/view/src/App.js
+++ app/view/src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
-
+import React from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages';
+import About from './pages/about';
+import Products from './pages/products';
+import Teams from './pages/team';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
+import SignIn from './pages/signin';
+  
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route  exact path='/about' element={<About/>} />
+        <Route  exact path='/products' element={<Products/>} />
+        <Route  exact path='/team' element={<Teams/>} />
+        <Route  exact path='/blogs' element={<Blogs/>} />
+        <Route  exact path='/sign-up' element={<SignUp/>} />
+        <Route  exact path='/sign-in' element={<SignIn/>} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
-
+  
 export default App;
---
+++ app/view/src/pages/signin.js
@@ -0,0 +1,25 @@
+import React from 'react';
+  
+const SignUp = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Center',
+        alignItems: 'Center',
+        flexDirection: 'column'
+
+      }}
+    >
+      <h1>Form Sign In</h1>
+      <h1>Form Sign In</h1>
+      <h1>Form Sign In</h1>
+      <h1>Form Sign In</h1>
+      <h1>Form Sign In</h1>
+      <h1>Form Sign In</h1>
+      <h1>Form Sign In</h1>
+    </div>
+  );
+};
+  
+export default SignUp;
