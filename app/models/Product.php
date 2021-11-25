0bcd531d48d41b2ef7b24a94681a1ea2130cd333
Bùi Hữu Tiến Đạt <dat.bui197404648@hcmut.edu.vn>
2021-11-26 00:44:02 +0700

    Add product restapi

--- app/models/Product.php
+++ app/models/Product.php
@@ -37,6 +37,38 @@ class Product {
         return $result;
     }
 
+    public function create() {
+
+        $sql = $this->conn->prepare("INSERT INTO ". $this->table . "(pName,src,srcDetail,price,quantity,description,type,alt) VALUES 
+        (?, ?, ?, ?, ?, ?, ?, ?)
+        ");
+
+        $sql->bind_param("sssiisss", $this->pName, $this->src, $this->srcDetail, 
+        $this->price, $this->quantity, $this->description, $this->type, $this->alt);
+        $result = $sql->execute();
+        $sql->close();
+        return $result;
+    }
+
+    public function delete() {
+        $sql = "DELETE FROM " . $this->table . " where id=" . $this->id;
+        $result = $this->conn->query($sql);
+        return $result;
+    }
+
+    public function update() {
+
+        $sql = $this->conn->prepare("UPDATE ". $this->table . " set pName=?, src=?, srcDetail=?, price=?, quantity=?, description=?, 
+        type=?, alt=? where id=?
+        ");
+
+        $sql->bind_param("sssiisssi", $this->pName, $this->src, $this->srcDetail, 
+        $this->price, $this->quantity, $this->description, $this->type, $this->alt, $this->id);
+        $result = $sql->execute();
+        
+        $sql->close();
+        return $result;
+    }
 }
 
 ?>
