a8bdaf1cad151937f7398c3239217940c9d8dd89
thluanlx02 <nguyenthanhluan596@gmail.com>
2021-11-30 01:33:34 +0700

    add about page

--- app/view/src/App.js
+++ app/view/src/App.js
@@ -1,36 +1,45 @@
-import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductProvider from "./context/ProductManagement";
-import Sidebar from './components/admin/Sidebar'
-import routes from "./routes";
-// import Navbar from './components/Navbar';
+// import Sidebar from "./components/admin/Sidebar";
+// import routes from "./routes";
+import Navbar from './components/Navbar';
 // import Footer from './components/Footer';
 // import Home from './pages';
-// import About from './pages/about';
+import About from './pages/about/about.page';
+
 // import Products from './pages/products';
 // import Teams from './pages/team';
 // import Blogs from './pages/blogs';
 // import SignUp from './pages/signup';
 // import SignIn from './pages/signin';
 
-  
 function App() {
+  console.log('1');
   return (
     <ProductProvider>
-    <BrowserRouter>
-<Sidebar/>
-      <Routes>
-      {
-        [
-        ...routes.map((route, index) => 
-          <Route  exact path={route.path} element={route.component} key={index}></Route>
-        )
-        ]
-      }
+      <BrowserRouter>
+        {/* <Sidebar /> */}
+        <Navbar/>
+        <Routes>
+          {/* {[
+            ...routes.map((route, index) => (
+              <Route
+                exact
+                path={route.path}
+                element={route.component}
+                key={index}
+              ></Route>
+            )),
+          ]} */}
 
-      </Routes>
+          <Route path='/about' exact element={<About/>}>
+
+          </Route>
+        </Routes>
       </BrowserRouter>
-      </ProductProvider>
-  )}
-  
+    </ProductProvider>
+  );
+}
+
 export default App;
