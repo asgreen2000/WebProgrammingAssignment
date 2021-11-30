60283747a69d7f33d61be3ce087e7f40a7273071
baokhanhle123 <baokhanhle27082001@gmail.com>
2021-11-30 21:21:51 +0700

    Commit before merge

--- app/view/src/App.js
+++ app/view/src/App.js
@@ -1,56 +1,52 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductProvider from "./context/ProductManagement";
 import Login from './pages/common/Login';
 import adminRoutes from './pages/admin/AdminRoutes';
 import ProtectedRoute from './pages/common/ProtectedRoute';
 import MainRoutes from './pages/main/MainRoutes';
-import Homepage from './pages/news/homepage/Homepage';
-import SinglePost from './components/news/singlePost/SinglePost';
 
 function App() {
+  console.log('1');
   return (
     <ProductProvider>
-      <BrowserRouter>
+    <BrowserRouter>
 
-        <Routes>
-          <Route path='login' element={<Login />} />
-
-          <Route path='news' element={<Homepage />} />
-          <Route path='news/post/1' element={<SinglePost />} />
-
-          <Route path='admin'>
-
-            {
-
-              adminRoutes.map(route => {
-
-                return <Route path={route.path} element={
-                  route.component
-                } />
-              })
-            }
-
-          </Route>
-
-
-          <Route path=''>
-            {
-              MainRoutes.map(route => {
-
-                return <Route path={route.path} element={
-                  route.component
-                } />
-              })
-            }
-          </Route>
-
-
-        </Routes>
-
+      <Routes>
+        <Route path='login' element={<Login />}/>
+      
+        <Route path='admin'>
+            
+          { 
+            
+            adminRoutes.map(route => {
+              
+              return <Route path={route.path} element={
+                route.component
+              }/>
+            })
+          }
+         
+        </Route>
+        
+        
+        <Route path=''>
+          {
+            MainRoutes.map(route => {
+              
+              return <Route path={route.path} element={
+                route.component
+              }/>
+            })
+          }
+        </Route>
+        
+      
+      </Routes>
+     
       </BrowserRouter>
     </ProductProvider>
-  )
+  );
 }
 
 export default App;
